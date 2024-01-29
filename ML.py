83b61c1c051fb8bdcaf250a6bb2065fa49dea3c2
Siyi Xu <emmaxu2005@gmail.com>
2024-01-29 23:38:13 +0800

    final project Data_Minds

--- ML.py
+++ ML.py
@@ -1,22 +1,26 @@
 # Import necessary libraries
 ## We use sklearn library for RandomForestRegressor implementation
-
+# Import necessary libraries
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 
 # Load the dataset
-filepath = 'data\catA_train.csv'
+filepath = 'data/catA_train.csv'
 dataset = pd.read_csv(filepath)
+
 # Modification of Dataset & EDA
-columns_to_drop = ["error", "Fiscal Year End", "Sales (Global Ultimate Total USD)", "Web Address","Square Footage", "Company Description", "PostCode",
-                   "8-Digit SIC Code", "8-Digit SIC Description", "AccountID",
-                   "Parent Company", "City", "Country", "Address", "Address1", "Industry", "Region", "Parent Country", "Global Ultimate Country", "Company"]
+columns_to_drop = ["AccountID","Company","Industry","8-Digit SIC Code","8-Digit SIC Description","Entity Type","Parent Company","Parent Country","Ownership Type","Company Description","Sales (Global Ultimate Total USD)","Fiscal Year End","Global Ultimate Company","Global Ultimate Country","Domestic Ultimate Company"]
 
 dataset = dataset.drop(columns=[col for col in columns_to_drop if col in dataset.columns], errors='ignore')
+dataset = dataset[dataset["Company Status (Active/Inactive)"] == "Active"]
+dataset["Import/Export Status"] = dataset["Import/Export Status"].replace({'': '0','Imports':1, 'Exports':2,'Both Imports & Exports': 3})
+dataset = dataset.drop(["Company Status (Active/Inactive)"], axis=1)
+
 
 # Extract features and target variable
 X = dataset.drop(['Sales (Domestic Ultimate Total USD)'], axis=1)  # Features
@@ -40,48 +44,55 @@ MLmodel.fit(X_train, y_train)
 
 # Make predictions on the test set
 y_pred = MLmodel.predict(X_test)
+result = list(y_pred)
 
 # Evaluate the model
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
+print("predicted domestic sales figures: ",result)
 print(f'Mean Squared Error: {mse}')
 print(f'R-squared Score: {r2}')
 
 
 
 
-
 def testing_hidden_data(hidden_data: pd.DataFrame) -> list:
-    result = [] 
-    
-    file_path = 'data\catA_train.csv'
-    dataset = pd.read_csv(file_path)
+    dataset = hidden_data
+    # Modification of Dataset & EDA
+    columns_to_drop = ["AccountID","Company","Industry","8-Digit SIC Code","8-Digit SIC Description","Entity Type","Parent Company","Parent Country","Ownership Type","Company Description","Sales (Global Ultimate Total USD)","Fiscal Year End","Global Ultimate Company","Global Ultimate Country","Domestic Ultimate Company"]
+
+    dataset = dataset.drop(columns=[col for col in columns_to_drop if col in dataset.columns], errors='ignore')
+    dataset = dataset[dataset["Company Status (Active/Inactive)"] == "Active"]
+    dataset["Import/Export Status"] = dataset["Import/Export Status"].replace({'': '0','Imports':1, 'Exports':2,'Both Imports & Exports': 3})
+    dataset = dataset.drop(["Company Status (Active/Inactive)"], axis=1)
+
+
+    # Extract features and target variable
     X = dataset.drop(['Sales (Domestic Ultimate Total USD)'], axis=1)  # Features
     y = dataset['Sales (Domestic Ultimate Total USD)']  # Target variable
 
+
+    ### ML: Random Forest Regressor
+    # Split the dataset into training and testing sets
+    # Using 20% data for testing and 80% data for training
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-
-    model = Pipeline([
+    # Create a pipeline with preprocessing (standardization) and the Random Forest Regressor
+    MLmodel = Pipeline([
         ('scaler', StandardScaler()),  # Standardize features
         ('regressor', RandomForestRegressor())  # Random Forest Regressor
     ])
 
     # Train the model
-    model.fit(X_train, y_train)
+    MLmodel.fit(X_train, y_train)
 
     # Make predictions on the test set
-    y_pred = model.predict(X_test)
-
-    # Evaluate the model
-    mse = mean_squared_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
-
-    print(f'Mean Squared Error: {mse}')
-    print(f'R-squared Score: {r2}')
-    
+    y_pred = MLmodel.predict(X_test)
+    result = list(y_pred)
     return result
+
 
 ##################
 #Test_Hidden_Data#
